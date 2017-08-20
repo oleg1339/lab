703a51ed152e6ef9d8e893817dee9595dc8f2bea
oleg1339 <31177684+oleg1339@users.noreply.github.com>
2017-08-20 12:28:05 +0300

    Add files via upload

---
+++ lab1.2.php
@@ -0,0 +1,23 @@
+<?php
+
+$boolean = true;
+echo '$boolean - ';
+var_dump($boolean);
+echo "<br/>";
+
+$string = 'test string';
+echo '$string -  ';
+var_dump($string);
+echo "<br/>";
+
+$float = 42.11;
+echo '$float - ';
+var_dump($float);
+echo "<br/>";
+
+$integer = 12;
+echo '$integer - ';
+var_dump($integer);
+echo "<br/>";
+
+
